f3a6ff51e41413fe58dfc3eee604becaf86ed2df
Alex Vong <chickenwingli69@gmail.com>
2021-04-16 16:01:07 +0100

    Finished V1

--- GPSMapper.py
+++ GPSMapper.py
@@ -198,10 +198,12 @@ class MapGUI:
         #rescale axes
         ax1.relim()
         ax1.autoscale_view()
-        #ax1.axis('off')
+        ax1.axis('off')
         fig1.canvas.draw()
-        #scale = 1.5
-        #f = zoom_factory(plot1,base_scale = scale)
+        scale = 1.5
+        zp = ZoomPan()
+        fzoom = zp.zoom_factory(ax1,base_scale = scale)
+        fpan = zp.pan_factory(ax1)
         
         """display info on click"""
         def on_click(event):
@@ -379,6 +381,7 @@ def closest_point_coords(ckdtree, x, y):
     # returns coordinates of closest point
     return ckdtree.data[closest_point_id(ckdtree, x, y)]
     # ckdtree.data is the same as points
+
 '''
 #zooming with mouse wheel
 def zoom_factory(ax,base_scale = 2.):
@@ -411,12 +414,93 @@ def zoom_factory(ax,base_scale = 2.):
     # attach the call back
     fig.canvas.mpl_connect('scroll_event',zoom_fun)
     #return the function
-    return zoom_fun
-''' 
+'''
+
+class ZoomPan:
+    def __init__(self):
+        self.press = None
+        self.cur_xlim = None
+        self.cur_ylim = None
+        self.x0 = None
+        self.y0 = None
+        self.x1 = None
+        self.y1 = None
+        self.xpress = None
+        self.ypress = None
+
+
+    def zoom_factory(self, ax, base_scale = 2.):
+        def zoom(event):
+            # get the current x and y limits
+            cur_xlim = ax.get_xlim()
+            cur_ylim = ax.get_ylim()
+            cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
+            cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
+            xdata = event.xdata # get event x location
+            ydata = event.ydata # get event y location
+            if event.button == 'up':
+                # deal with zoom in
+                scale_factor = 1/base_scale
+            elif event.button == 'down':
+                # deal with zoom out
+                scale_factor = base_scale
+            else:
+                # deal with something that should never happen
+                scale_factor = 1
+                print(event.button)
+            # set new limits
+            ax.set_xlim([xdata - cur_xrange*scale_factor,
+                         xdata + cur_xrange*scale_factor])
+            ax.set_ylim([ydata - cur_yrange*scale_factor,
+                         ydata + cur_yrange*scale_factor])
+            plt.draw() # force re-draw
+    
+        fig = ax.get_figure() # get the figure of interest
+        # attach the call back
+        fig.canvas.mpl_connect('scroll_event',zoom)
+        #return the function
+        return zoom
+
+    def pan_factory(self, ax):
+        def onPress(event):
+            if event.inaxes != ax: return
+            self.cur_xlim = ax.get_xlim()
+            self.cur_ylim = ax.get_ylim()
+            self.press = self.x0, self.y0, event.xdata, event.ydata
+            self.x0, self.y0, self.xpress, self.ypress = self.press
+
+        def onRelease(event):
+            self.press = None
+            ax.figure.canvas.draw()
+
+        def onMotion(event):
+            if self.press is None: return
+            if event.inaxes != ax: return
+            dx = event.xdata - self.xpress
+            dy = event.ydata - self.ypress
+            self.cur_xlim -= dx
+            self.cur_ylim -= dy
+            ax.set_xlim(self.cur_xlim)
+            ax.set_ylim(self.cur_ylim)
+
+            ax.figure.canvas.draw()
+
+        fig = ax.get_figure() # get the figure of interest
+
+        # attach the call back
+        fig.canvas.mpl_connect('button_press_event',onPress)
+        fig.canvas.mpl_connect('button_release_event',onRelease)
+        fig.canvas.mpl_connect('motion_notify_event',onMotion)
+
+        #return the function
+        return onMotion
+
+
+
 #set up graph
 fig1, ax1 = plt.subplots()
 fig1.set_size_inches(8, 6)
-#ax1.axis('off')
+ax1.axis('off')
 
 #set up GUI
 window = tk.Tk()
@@ -426,8 +510,8 @@ window.title("GPS Mapper")
 mapgui = MapGUI(window)
 canvas = FigureCanvasTkAgg(fig1, window)   
 canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-toolbar = NavigationToolbar2Tk(canvas, window) 
-toolbar.update() 
+#toolbar = NavigationToolbar2Tk(canvas, window) 
+#toolbar.update() 
 window.mainloop()
     
 
